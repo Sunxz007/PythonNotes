b49b50f0208a925ef3735dce4d1b40520e70deb8
Sunxz007 <977622085@qq.com>
2019-10-29 10:48:54 +0800

    Python 基础

---
+++ code/Fibonacci.py
@@ -0,0 +1,12 @@
+# -*- coding: utf8 -*-
+'''
+@Description: 0b
+@Author: 孙晓昼
+@since: 2019-10-28 16:01:19
+@LastEditors: 孙晓昼
+@LastEditTime: 2019-10-28 16:33:24
+'''
+a,b=0,1
+while b<1000000:
+    print(b,end=',')
+    a,b=b,a+b
